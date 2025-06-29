725864eef0814bd74ae90be9d8be1030db5fbdf4
chauhan22adarsh <adarsh22chauhan@gmail.com>
2025-06-29 22:14:26 +0530

    changes made for render

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
-const listing=require("./models/listing.js");
-const port=3000;
-const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
-const path=require("path");
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate");
-const wrapAsync=require("./utils/wrapAsync.js");
-const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema}=require("./schema.js");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
+require("dotenv").config();
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 
 async function main() {
   try {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("✅ Connected to MongoDB");
 
     const count = await listing.countDocuments();
@@ -39,76 +38,105 @@ async function main() {
 
 main();
 
-const validateListing=(req,res,next)=>{
-  let {error}=listingSchema.validate(req.body);
-  if(error){
-    let errMsg=error.details.map((el)=>el.message).join(",");
-    throw new ExpressError(400,errMsg);
-  }else{next();}
-}
-
-app.get("/",(req,res)=>{
-    res.render("listings/home.ejs");
-})
-
-app.get("/listings",wrapAsync(async (req,res)=>{
-  const allListings=await listing.find({});
-  res.render("listings/index.ejs",{allListings})
-}))
-
-app.get("/listings/new",(req,res)=>{
-  res.render("listings/new.ejs")
-})
-
-app.get("/listings/:id",wrapAsync(async (req,res)=>{
-  let {id}=req.params;
-  const sting=await listing.findById(id);
-  res.render("listings/show.ejs",{sting})
-}))
-
-app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
-  let {id}=req.params;
-  const sting=await listing.findById(id);
-  res.render("listings/edit.ejs",{sting})
-}))
-
-app.post("/listings",validateListing,wrapAsync(async (req,res)=>{
-  const newListing=new listing(req.body.listing);
-  await newListing.save();
-   res.redirect("/listings");
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
   }
-))
+};
 
-app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
-  let {id}=req.params;
-  let data=req.body.listing;
-  data.image.filename="listingimage";
-  await listing.findByIdAndUpdate(id,{...data},{runValidators:true,new:true});
-  res.redirect(`/listings/${id}`);
-}))
+app.get("/", (req, res) => {
+  res.render("listings/home.ejs");
+});
 
-app.delete("/listings/:id",wrapAsync(async (req,res)=>{
-  let {id}=req.params;
-  await listing.findByIdAndDelete(id);
-  res.redirect("/listings");
-}))
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListings = await listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
 
-app.all("*",(req,res,next)=>{
-  throw new ExpressError(404,"Page Not Found!");
-})
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
 
-app.use((err,req,res,next)=>{
-    if(err.name=="TypeError"){
-        throw new ExpressError(500,"TypeError: Please check your input and try again.")
-    }else{
-    next(err);}
-})
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const sting = await listing.findById(id);
+    res.render("listings/show.ejs", { sting });
+  })
+);
 
-app.use((err,req,res,next)=>{
-  let {statusCode=500,message="something went wrong"}=err;
-  res.status(statusCode).render("listings/error.ejs",{message});
-})
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const sting = await listing.findById(id);
+    res.render("listings/edit.ejs", { sting });
+  })
+);
 
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`)
-})
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    const newListing = new listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
+
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let data = req.body.listing;
+    data.image.filename = "listingimage";
+    await listing.findByIdAndUpdate(
+      id,
+      { ...data },
+      { runValidators: true, new: true }
+    );
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  throw new ExpressError(404, "Page Not Found!");
+});
+
+app.use((err, req, res, next) => {
+  if (err.name == "TypeError") {
+    throw new ExpressError(
+      500,
+      "TypeError: Please check your input and try again."
+    );
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrong" } = err;
+  res.status(statusCode).render("listings/error.ejs", { message });
+});
+
+app.listen(process.env.port, () => {
+  console.log(`listening on port ${process.env.port}`);
+});
--- init/index.js
+++ init/index.js
@@ -1,19 +1,17 @@
-const mongoose=require("mongoose");
-const initData=require("./data.js")
-const listing=require("../models/listing.js");
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const listing = require("../models/listing.js");
 
 main()
-.then(()=>{console.log("connected successfully")})
-.catch(err => console.log(err));
+  .then(() => {
+    console.log("connected successfully");
+  })
+  .catch((err) => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
-}
-
-const initDB=async ()=>{
-    await listing.deleteMany({});
-    await listing.insertMany(initData.data);
-    console.log("data was initialized");
-}
+const initDB = async () => {
+  await listing.deleteMany({});
+  await listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
 
 initDB();
